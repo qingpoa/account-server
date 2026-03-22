180f61cf88d08c86eb957f75828a3117419f0028
qingpoa <senqibusenqi@163.com>
2026-03-22 21:45:00 +0800

    初始化项目

---
+++ account-server/src/main/java/com/qingpo/AccountServerApplication.java
@@ -0,0 +1,13 @@
+package com.qingpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountServerApplication.class, args);
+    }
+
+}
---
+++ account-server/src/test/java/com/qingpo/AccountServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.qingpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("测试");
+    }
+
+}
