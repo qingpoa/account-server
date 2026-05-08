5aa5a7a073afde709ab171c18b79fca179fc7d46
qingpoa <senqibusenqi@163.com>
2026-05-09 00:48:00 +0800

    修改了https跨域问题

--- account-server/src/main/java/com/qingpo/config/WebMvcConfig.java
+++ account-server/src/main/java/com/qingpo/config/WebMvcConfig.java
@@ -25,7 +25,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:*", "http://127.0.0.1:*")
+                .allowedOriginPatterns(
+                        "http://localhost:*",
+                        "http://127.0.0.1:*",
+                        "https://localhost:*",
+                        "https://127.0.0.1:*",
+                        "http://zhangbenheizi.cn",
+                        "https://zhangbenheizi.cn"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("Content-Disposition")
